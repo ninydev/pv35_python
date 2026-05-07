dd1b956a2995eda1a59fde99d6667af34cc2a55b
Oleksandr Nykytin <alex_n@alma-soft.com>
2026-05-07 19:27:14 +0300

    dict

---
+++ 2026_05_07/dict.py
@@ -0,0 +1,51 @@
+
+
+usersCred = {
+    'keeper@ninydev.com': '123456',
+    'onykytin@gerc,ua': 'qwerty',
+    'nikitin_a@itstep.academy': 'password'
+}
+
+print(usersCred)
+
+print(usersCred.keys())
+print(usersCred.values())
+
+# usersCred.update({
+#     'nika_web@ukr.net': 'some_passwd'
+# })
+#
+# usersCred.update({
+#     'keeper@ninydev.com': 'new_password'
+# })
+#
+# print (usersCred['keeper@ninydev.com'])
+# print(usersCred.get('onykytin@gerc,ua'))
+# print(usersCred.get('non_existent_key', 'Default Value'))
+#
+# usersCred['phenix.ods@gmail.com'] = 'Qwe'
+#
+# print(usersCred)
+
+
+#---------------------------------------------------------------------
+
+# emails = [ 'keeper@ninydev.com', 'onykytin@gerc,ua', 'nikitin_a@itstep.academy']
+# paswd = ['123456', 'qwerty', 'password']
+#
+#
+# # print('Array (List) Version')
+# # for i in range(len(emails)):
+# #     print(i, emails[i], paswd[i])
+#
+# # Як додати нового
+# emails.append('keeper@ninydev.com')
+# paswd.append('new_password')
+# # Як шукати
+# index = emails.index('onykytin@gerc,ua')
+# print(f"Index of 'onykytin@gerc,ua': {index}", f"passwd: {paswd[index]} " )
+#
+# print(emails)
+# print(paswd)
+
+#---------------------------------------------------------------------
