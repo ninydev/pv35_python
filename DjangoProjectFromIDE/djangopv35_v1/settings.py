7280e66e496728c9da1aac22a73737384ff5bd97
Oleksandr Nykytin <alex_n@alma-soft.com>
2026-06-06 16:15:56 +0300

    Django project start app

--- DjangoProjectFromIDE/djangopv35_v1/settings.py
+++ DjangoProjectFromIDE/djangopv35_v1/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import accounts
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pv35.apps.Pv35Config',
+    'pages',
+    'accounts'
 ]
 
 MIDDLEWARE = [
