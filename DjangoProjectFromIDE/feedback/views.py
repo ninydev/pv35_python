15e26ed44eed75d125d221828683efa73174ac39
Oleksandr Nykytin <alex_n@alma-soft.com>
2026-06-13 15:53:54 +0300

    Simple Form

--- DjangoProjectFromIDE/feedback/views.py
+++ DjangoProjectFromIDE/feedback/views.py
@@ -5,5 +5,12 @@ from feedback.forms import FeedbackForm
 
 # Create your views here.
 def feedback_page(request):
+
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
     form = FeedbackForm()
     return render(request, 'forms/feedback_page.html', {'form': form})
