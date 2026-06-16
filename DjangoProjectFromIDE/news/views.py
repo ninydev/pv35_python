9d88d524374110a1e23e9aba08c3ae36e846d0cb
Oleksandr Nykytin <alex_n@alma-soft.com>
2026-06-16 19:23:04 +0300

    Add Paginate

--- DjangoProjectFromIDE/news/views.py
+++ DjangoProjectFromIDE/news/views.py
@@ -6,8 +6,17 @@ from news.models import News
 
 # Create your views here.
 def news_list(request):
-    news_list = News.objects.all().order_by('-created_at')
-    return render(request, 'news/news_list.html', {'news_list': news_list})
+
+    page = request.GET.get('page', 1)
+    per_page = request.GET.get('per_page', 6)
+    # news_list = News.objects.all().order_by('-created_at')
+    offset = (page - 1) * per_page
+
+    news_list = News.objects.all().order_by('-created_at')[offset:offset + per_page]
+    return render(request, 'news/news_list.html',
+                  {'news_list': news_list,
+                   'paginate': {'page': page, 'per_page': per_page}
+                   })
 
 def news_list_api(request):
     news_list = News.objects.all().order_by('-created_at')
